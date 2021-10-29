774eab2ef5392f062445e647dee9c3a83a47930e
Rick <rick@chunker.com>
2021-10-29 08:56:10 -0600

    Add business photo.

--- src/components/Marquee/index.jsx
+++ src/components/Marquee/index.jsx
@@ -1,16 +1,26 @@
 import React from "react";
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
-import BackgroundImage from "../../assets/images/charisa-zions.jpeg";
+import BusinessImage from "../../assets/images/charisa-business-photo.JPG";
+import Box from "@mui/material/Box";
 
 export default function PaperSheet() {
   return (
-    <Grid sx={{
-      background: `url(${BackgroundImage}) no-repeat;`,
+    <Grid container direction="row">
+    <Grid xs={4} sx={{
+      background: `url(${BusinessImage}) no-repeat;`,
       height: "800px",
-      maxHeight: "100vh",
-      backgroundSize: "cover",
+      maxHeight: "80vh",
+      backgroundPosition: "left top",
+      backgroundSize: "100%"
     }}
+      container
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+    >
+    </Grid>
+    <Grid xs={8} sx={{bgcolor: 'primary.main'}}
       container
       direction="column"
       alignItems="center"
@@ -31,6 +41,7 @@ export default function PaperSheet() {
         Seeking PMHNP Clinical Preceptorship - Fall 2022
         </Typography>
       </Grid>
+    </Grid>
     </Grid>
   );
 }
